24afeb75622b520c0d9becfcf114fae5ac7048d6
Sohail Mehmood <sohail.mehmood@worldline.com>
2022-09-07 13:37:01 +0100

    Add project files.

---
+++ ConcatenatesLists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConcatenatesLists;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args == null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        List<string> letters = new()
+        {
+            "a",
+            "b",
+            "c"
+        };
+
+        List<string> numbers = new()
+        {
+            "1",
+            "2",
+            "3"
+        };
+
+        letters.AddRange(numbers);
+        Console.WriteLine(String.Join(",", letters));
+    }
+}
